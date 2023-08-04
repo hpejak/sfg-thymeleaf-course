1bc1aa460f7a83bb28f295ec07cbcc52ac30303b
Pejak <pejak.hrvoje@gmail.com>
2023-08-04 18:29:51 +0200

    init

---
+++ src/main/java/hr/pejak/sfgthymeleafcourse/SfgThymeleafCourseApplication.java
@@ -0,0 +1,13 @@
+package hr.pejak.sfgthymeleafcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgThymeleafCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgThymeleafCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/pejak/sfgthymeleafcourse/SfgThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.pejak.sfgthymeleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgThymeleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
