e53bd2fd3085c4422a4d3e5cff5a0ab965fa1fa2
Pejak <pejak.hrvoje@gmail.com>
2023-08-21 22:15:04 +0200

    dynamic Content

--- src/main/java/hr/pejak/sfgthymeleafcourse/controllers/ProductController.java
+++ src/main/java/hr/pejak/sfgthymeleafcourse/controllers/ProductController.java
@@ -1,13 +1,33 @@
 package hr.pejak.sfgthymeleafcourse.controllers;
 
+import hr.pejak.sfgthymeleafcourse.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
 
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/product")
     public String getProduct() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProductById(@PathVariable Integer id, Model model){
+
+        model.addAttribute("product", productService.getProduct(id));
+
         return "product";
     }
 
